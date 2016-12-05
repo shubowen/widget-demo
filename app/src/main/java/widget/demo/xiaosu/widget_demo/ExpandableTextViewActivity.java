bf7ac9ddb9fc31b1074185e632b6a03eefd8d11c
shubowen <465560907@qq.com>
2016-12-05 18:03:49 +0800

    添加ExpandableTextView

---
+++ app/src/main/java/widget/demo/xiaosu/widget_demo/ExpandableTextViewActivity.java
@@ -0,0 +1,12 @@
+package widget.demo.xiaosu.widget_demo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 疏博文 新建于 16/12/5.
+ * 邮箱：shubw@icloud.com
+ * 描述：请添加此文件的描述
+ */
+
+public class ExpandableTextViewActivity extends AppCompatActivity {
+}
