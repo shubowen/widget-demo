cfb3008fb9cbca2ac1ddfe7d1b7dac63b5225d18
shubowen <shubw@icloud.com>
2016-12-05 22:54:55 +0800

    修复ExpandableTextView折叠问题

--- app/src/main/java/widget/demo/xiaosu/widget_demo/ExpandableTextViewActivity.java
+++ app/src/main/java/widget/demo/xiaosu/widget_demo/ExpandableTextViewActivity.java
@@ -1,5 +1,7 @@
 package widget.demo.xiaosu.widget_demo;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,4 +11,10 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class ExpandableTextViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_expandable_text);
+    }
 }
