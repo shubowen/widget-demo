833f8c024132d338d60ab401374bd1940a6700eb
shubowen <465560907@qq.com>
2016-12-06 09:46:20 +0800

    移除不必要文件

--- lib.widget/src/main/java/com/xiaosu/lib/base/widget/ExpandableTextView.java
+++ lib.widget/src/main/java/com/xiaosu/lib/base/widget/ExpandableTextView.java
@@ -26,7 +26,6 @@ import android.os.Build;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.text.StaticLayout;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.SparseBooleanArray;
